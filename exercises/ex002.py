745677d858f882540626fba290676c61f98a936c
Daniel <107972303+danielvazcalaca@users.noreply.github.com>
2024-09-06 08:55:33 -0300

    add 005

--- exercises/ex002.py
+++ exercises/ex002.py
@@ -1,1 +1,20 @@
-qq
+# for i in range(10, 4, -1):
+#     print(i)
+
+
+# for banana in range(10):
+#     print(banana)
+
+# for x in range(10, 16, 2):
+#     print(x)
+#
+# for y in range(100, 350, 50):
+#     print(y)
+#
+# for z in range(-100, -350, -50):
+#     print(z)
+
+
+# for n in range(12, 16):
+#     print(n)
+
---
+++ exercises/ex003.py
@@ -0,0 +1,12 @@
+import sys
+
+while True:
+    print("Say your name")
+    name = input()
+    if name == ("Abobora"):
+        print("Congrats")
+        break
+    elif name == ("exit"):
+        sys.exit()
+
+print("???")
