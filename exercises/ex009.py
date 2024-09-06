4c057089cb9afd6c7ddf39a0285d364748f24e56
Daniel <107972303+danielvazcalaca@users.noreply.github.com>
2024-09-06 16:17:08 -0300

    Add ex009

--- exercises/ex009.py
+++ exercises/ex009.py
@@ -1,4 +1,12 @@
-def abobora():
-    print("asdasdw")
+# def abobora():
+#     print("asdasdw")
+#
+# abobora()
 
-abobora()
+
+name = "Daniel"
+number = 123
+
+def hello(name, number):
+    print("Hello " + name + str(number) ), print(name) ,print(number)
+hello(name, number)
