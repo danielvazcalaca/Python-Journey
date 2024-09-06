384a30b1ba5862f9da89d123b51936f69fcd8de2
Daniel <107972303+danielvazcalaca@users.noreply.github.com>
2024-09-06 17:44:59 -0300

    Add ex011

---
+++ exercises/ex011.py
@@ -0,0 +1,17 @@
+# print(None)
+
+# spam = print("hi")
+# print(None == spam)
+# print(None != spam)
+
+# def print():
+#     input("test")
+#
+# print()
+
+
+def abobora():
+    limao = 123
+abobora()
+print(limao)
+# o escopo acima não é global então ao tentar imprimir a variavel de dentro da função, da erro
