0cce6f2252689021f0890bc9ee301eaa55a0987b
Daniel <107972303+danielvazcalaca@users.noreply.github.com>
2024-09-11 17:46:06 -0300

    add ex013

--- exercises/ex013.py
+++ exercises/ex013.py
@@ -1,5 +1,14 @@
 abobora = ["asdw", "asdwdas", "asdasdw"]
 print(abobora[-1])
 
-limao = [[123, 12, 98], ["123", 456, "zxczxcx"], "123123123"]
+limao = [[123, 12, 98], ["123", 456, "zxczxcx"], "123123123", "agua"]
 print(limao[0][-1])
+print(abobora[0:3])
+print(len(abobora))
+
+limao[-1] = "Final"
+print(limao)
+
+print(limao + abobora)
+del abobora[2]
+print(abobora)
